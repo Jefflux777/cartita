f028cb3ab759a48181b392d1f1e6ac006b0b0cef
Jefflux777 <sicajau.jefferson.ipcs@gmail.com>
2025-02-01 23:12:16 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('GRACIAS POR ACEPTAR SER MI SAN VALENTIN TQM').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
